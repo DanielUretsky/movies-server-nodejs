4fe4b814f0806ef5d61570a61b327c3f75c9c760
Daniel <daniel353a3@gmail.com>
2024-03-07 23:38:00 +0200

    Created auth router, with 2 routes registation/login

---
+++ routers/authRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const authBLL = require('../BLL/authBLL');
+
+router.post('/registration', async(req, res) => {
+    try {
+        const userData = req.body;
+        const response = await authBLL.registration(userData);
+        if(!response) res.send(500).send('Unknown server error');
+
+        res.status(response.status).send(response.message);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Unknown server error');
+    }
+});
+
+router.post('/login', async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        const response = await authBLL.login(email, password);
+        if(!response) res.send(500).send('Unknown server error');
+
+        res.status(response.status).send(response.message);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Unknown server error');
+    }
+});
+
+module.exports = router;
