32627c2999af507f71798695f9cdf6da450e1e19
Daniel <daniel353a3@gmail.com>
2024-03-11 17:51:51 +0200

    some changes

--- main.js
+++ main.js
@@ -4,29 +4,26 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const tokenMiddleware = require('./middlewares/tokenMiddleware');
-const userRequestLogMiddleware = require('./middlewares/userRequestLogMiddleware');
 
 const connectToDB = require('./configs/connectToDB');
 const cors = require('cors');
 
-const authRouter = require('./routers/authRouter')
-const usersRouter = require('./routers/usersRouter')
-
+const authRouter = require('./routers/authRouter');
+const usersRouter = require('./routers/usersRouter');
+const userRequestLogMiddleware = require('./middlewares/userRequestLogMiddleware');
 
 //configs
 connectToDB();
 
-//middlewares
+//middlewares 
 app.use(express.json());
 app.use(cors());
 
-
 //routes
 app.use('/auth', authRouter, userRequestLogMiddleware);
-app.use('/users', tokenMiddleware, usersRouter, userRequestLogMiddleware)
-
+app.use('/users', tokenMiddleware, usersRouter, userRequestLogMiddleware);
 
 //listen 
 app.listen(PORT, () => {
     console.log(`Server is running: http://localhost:${PORT}`);
-});
+}); 
