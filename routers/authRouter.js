2ccce64f917b49ffb302e3b0647d4107d5e0e462
Daniel <daniel353a3@gmail.com>
2024-03-14 03:05:37 +0200

    added user parameter to session if it doesnt exist and status code starts from 2

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,28 +1,37 @@
 const express = require('express');
 const router = express.Router();
-
 const authBLL = require('../BLL/authBLL');
 
-router.post('/registration', async(req, res) => {
+router.post('/registration', async(req, res, next) => {
+    const userData = req.body;
     try {
-        const userData = req.body;
         const response = await authBLL.registration(userData);
-        if(!response) res.send(500).send('Unknown server error');
+        if(!response) res.status(500).send('Unknown server error');
 
         res.status(response.status).send(response.message);
         next();
     } catch (err) {
+        console.log(err.name);
         console.log(err.message);
         res.status(500).send('Unknown server error');
     }
 });
 
-router.post('/login', async(req, res) => {
+router.post('/login', async(req, res, next) => {
     try {
         const {email, password} = req.body;
         const response = await authBLL.login(email, password);
         if(!response) res.send(500).send('Unknown server error');
-
+        //console.log(response);
+        
+        if(!req.session.user && response.status.toString()[0] == '2') {
+            req.session.user = {
+                ...response.message.userInfo,
+                numberOfRequests: 10,
+            }
+            req.session.save();  
+        }
+        
         res.status(response.status).send(response.message);
         next();
     } catch (err) {
