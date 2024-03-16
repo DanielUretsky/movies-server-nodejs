9b3d7dd1150e451156480bed25ad7e15898e8860
Daniel <daniel353a3@gmail.com>
2024-03-16 02:26:12 +0200

    created DLL/DAL to read and write into json

--- DLL/requestCountLogDLL.js
+++ DLL/requestCountLogDLL.js
@@ -1,3 +1,14 @@
 const jsonfile = require('jsonfile');
 
- 
+const getRequestsCountFile = async() => {
+    return jsonfile.readFile('./log/requestsCount.log.json');
+}
+
+const writetRequestsCountFile = async(logObj) => {
+    return jsonfile.writeFile('./log/requestsCount.log.json', logObj);
+}
+
+module.exports = {
+    getRequestsCountFile,
+    writetRequestsCountFile
+}
