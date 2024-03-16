1d2cd73443ae1deb2792bcead99e62759454f32b
Daniel <daniel353a3@gmail.com>
2024-03-16 02:14:50 +0200

    some changes

--- utils/userRequstLogUtils.js
+++ utils/userRequstLogUtils.js
@@ -1,48 +1,41 @@
-const setUser = (userId, potentialUserInfo) => {
-    return (userId || potentialUserInfo) ? userId || potentialUserInfo : 'Unknown';
-}
-
 const setNumberOfRequests = (user, reqOriginalUrl, resStatusCode) => {
-    //console.log(reqOriginalUrl.includes('/registration') && user && resStatusCode[0] != '2');
-
-    console.log(resStatusCode);
     let reqBefore = "";
     let reqAfter = "";
+  
     switch (true) {
-        case reqOriginalUrl.includes('/registration') && !user && resStatusCode[0] == '2':
+        case reqOriginalUrl.includes('/registration') && Boolean(!user) && resStatusCode[0] == '2':
             reqBefore = 10;
             reqAfter = 10;
             return [reqBefore, reqAfter];
 
-        case reqOriginalUrl.includes('/registration') && user && !resStatusCode[0] != '2':
+        case reqOriginalUrl.includes('/registration') && Boolean(user) && !resStatusCode[0] != '2':
             reqBefore = user.numberOfRequests;
             reqAfter = user.numberOfRequests;
             return [reqBefore, reqAfter];
 
-        case (reqOriginalUrl.includes('/login') && !user) || (reqOriginalUrl.includes('/registration') && !user):
+        case (reqOriginalUrl.includes('/login') && Boolean(!user)) || (reqOriginalUrl.includes('/registration') && Boolean(!user)):
             return [reqBefore, reqAfter];
 
-        case reqOriginalUrl.includes('/login') && user:
+        case reqOriginalUrl.includes('/login') && Boolean(user):
             reqBefore = user.numberOfRequests;
             reqAfter = user.numberOfRequests;
             return [reqBefore, reqAfter];
 
-        case user && resStatusCode[0] == '2':
+        case Boolean(user) && resStatusCode[0] == '2':
             reqBefore = user.numberOfRequests + 1;
             reqAfter = user.numberOfRequests;
             return [reqBefore, reqAfter];
 
-        case user && resStatusCode[0] != '2':
+        case Boolean(user) && resStatusCode[0] != '2':
             reqBefore = user.numberOfRequests;
             reqAfter = user.numberOfRequests;
             return [reqBefore, reqAfter];
 
-        case !user :
+        case Boolean(!user) :
             return [reqBefore, reqAfter];   
     };
 }
 
 module.exports = {
-    setUser,
     setNumberOfRequests
 }
