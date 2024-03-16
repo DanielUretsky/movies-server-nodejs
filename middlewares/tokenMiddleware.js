3d42dd7a97a1f724afc1c0a44281729cd1f56bbe
Daniel <daniel353a3@gmail.com>
2024-03-16 02:19:46 +0200

    some changes

--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -1,29 +1,33 @@
-require('dotenv').config();
 const userRequestLogMiddleware = require('./userRequestLogMiddleware');
+const requestCountLogMiddleware = require('./requestCountLogMiddleware');
+
 const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if(!authHeader) res.status(401).send('Unauthorized!');
- 
-    const token = authHeader.split(' ')[1];
     try {
+        const authHeader = req.headers.authorization;
+        if(!authHeader) res.status(401).send('Unauthorized!');
+       
+        const token = authHeader.split(' ')[1];
         const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        
         req.user = user;
-        next();
+        return next();
     } catch (err) { 
         console.log(err.message);
         res.status(401).send('Unauthorized!');
         /* 
-            We need to complete log anyway, we cant do it if we dont call
-            our 'userRequestLogMiddleware' after 'authenticateToken' is failed 
-            and we can't call 'next' because we dont want to continue our routers logic.
-
-            So if we in block catch after sending response
-            (we do it after all, because we need info about 'req' and 'res' parameters) 
-            we call to 'userRequestLogMiddleware' to complete log 
+        We need to complete log anyway, we cant do it if we dont call
+        our 'userRequestLogMiddleware' after 'authenticateToken' is failed 
+        and we can't call 'next' because we dont want to continue our routers logic.
+        
+        So if we in block catch after sending response
+        (we do it after all, because we need info about 'req' and 'res' parameters) 
+        we call to 'userRequestLogMiddleware' to complete log 
         */
-        return userRequestLogMiddleware(req, res);
+        requestCountLogMiddleware(req, res);
+        userRequestLogMiddleware(req, res, next);
+        
     }
 } 
 
