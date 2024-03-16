71401ba7c148bca42b57c92881faf62692f4739f
Daniel <daniel353a3@gmail.com>
2024-03-16 02:19:22 +0200

    some changes

--- middlewares/userRequestLogMiddleware.js
+++ middlewares/userRequestLogMiddleware.js
@@ -1,20 +1,25 @@
 const userRequestLogDLL = require('../DLL/userRequestLogDLL');
 const userRequestLogUtils = require('../utils/userRequstLogUtils');
 
-const userRequestLog = async (req, res) => {
-  
-    // console.log('userRequestLog work');
-    // console.log(res.statusCode);
+const userRequestLog = async (req, res, next) => {
     try {
         // i get the response status code from previous function and converted him to string 
         // to get value of the first substr, so if the value is 2 that means our status code 
         // starts from 2, and that means our response is completes without errors, so it can be true or false
-        const requestCompleted = res.statusCode.toString()[0] == '2' ? `${true} statusCode: ${res.statusCode}` : `${false} statusCode: ${res.statusCode}`;
+        const requestCompleted = res.statusCode.toString()[0] == '2' ? 
+                             `${true} statusCode: ${res.statusCode}` : 
+                             `${false} statusCode: ${res.statusCode}`;
         const { requestLogs } = await userRequestLogDLL.getUserRequestLogFile();
 
-        const userSession = req.session.user;
+        const userSession = req.session?.user;
         // trying to get any important user info
-        const potentialUserInfo = req.body.email || req.user?.email || req.body.username || req.user?.username;
+        const potentialUserInfo = 
+            req.body.email || 
+            req.user?.email || 
+            req.body.username || 
+            req.user?.username || 
+            userSession.email ||  
+            userSession._id;
         
         const [reqBefore, reqAfter] = userRequestLogUtils.setNumberOfRequests(userSession, req.originalUrl, res.statusCode.toString());
         const requestLogObj = {
@@ -30,7 +35,8 @@ const userRequestLog = async (req, res) => {
 
         requestLogs.push(requestLogObj);
         await userRequestLogDLL.writetUserRequestLogFile({ requestLogs });
-       
+
+        next();
     } catch (err) {
         console.log(err);
         console.log(`log complition error: ${err.name} => ${err.message}`);
