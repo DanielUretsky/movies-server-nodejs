42b27c885bca3a36f4595515ea1b802bfa053b4f
Daniel <daniel353a3@gmail.com>
2024-03-22 19:44:25 +0200

    removed spaces

--- configs/connectToDB.js
+++ configs/connectToDB.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectToDB = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/movies-nodejs')
+    mongoose.connect(`${process.env.MONGODB_URI}`)
         .then(() => console.log('Database OK.'))
         .catch(err => console.log(err));
 }
 
-module.exports = connectToDB;
+module.exports = connectToDB;  
